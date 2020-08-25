21b08c7f27d5541bb647cffc104513dc97cd85c1
Zarva <zarva6596@gmail.com>
2020-08-25 15:44:04 +0300

    Updates

---
+++ scripts/main.js
@@ -0,0 +1,42 @@
+'use strict';
+
+const food = {
+  'Meat': {
+    'Pork': {},
+    'Beef': {},
+  },
+
+  'Fruit': {
+    'Red': {
+      'Cherry': {},
+      'Strawberry': {},
+    },
+    'Yellow': {
+      'Banana': {},
+      'Pineapple': {},
+    },
+
+  },
+};
+
+const tree = document.querySelector('#tree');
+
+function createTree(element, data) {
+  const ul = document.createElement('ul');
+
+  for (const key in data) {
+    const li = document.createElement('li');
+
+    li.innerText = key;
+
+    ul.append(li);
+
+    if (Object.keys(data[key].length > 0)) {
+      createTree(ul, data[key]);
+    }
+  }
+
+  element.append(ul);
+}
+
+createTree(tree, food);
